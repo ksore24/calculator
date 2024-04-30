742a2b5464af0d2c9072ac88313acf026f61a4fc
Kenny Sorensen <kenny.d.sorensen@gmail.com>
2024-04-30 15:07:21 -0400

    basic alignments and setup of calculator

---
+++ javascript.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container");
+
+const display = document.createElement("div");
+
+const buttonRow1 = document.createElement("div");
+const button7 = document.createElement("button");
+const button8 = document.createElement("button");
+const button9 = document.createElement("button");
+const buttonDivide = document.createElement("button");
+
+
+const buttonRow2 = document.createElement("div");
+const button4 = document.createElement("button");
+const button5 = document.createElement("button");
+const button6 = document.createElement("button");
+const buttonMultiply = document.createElement("button");
+
+
+const buttonRow3 = document.createElement("div");
+const button1 = document.createElement("button");
+const button2 = document.createElement("button");
+const button3 = document.createElement("button");
+const buttonSubtract = document.createElement("button");
+
+
+const buttonRow4 = document.createElement("div");
+const buttonClear = document.createElement("button");
+const button0 = document.createElement("button");
+const buttonDot = document.createElement("button");
+const buttonEquals = document.createElement("button");
+
+container.appendChild(display);
+container.appendChild(buttonRow1);
+container.appendChild(buttonRow2);
+container.appendChild(buttonRow3);
+container.appendChild(buttonRow4);
