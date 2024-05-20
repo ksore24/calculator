f3eb3053d0d2f0ab7199cce53458a2c8a2558255
Kenny Sorensen <kenny.d.sorensen@gmail.com>
2024-05-20 15:17:12 -0400

    basic logic of functions added

--- javascript.js
+++ javascript.js
@@ -1,15 +1,82 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let mathFunction = 0;
+let answer;
+
+const container = document.querySelector(".container");
 const display = document.querySelector(".display");
+
 const buttonEnter = document.querySelector(".buttonEnter");
-const buttonNumber = document.querySelector(".buttonNumber");
+buttonEnter.addEventListener("click", enterFunction);
+
+const buttonNumberList = document.querySelectorAll(".buttonNumber");
+for(i = 0; i < 10; i++)
+{
+    buttonNumberList[i].addEventListener("click", inputNumber);
+}
+
 const buttonDivide = document.querySelector(".buttonDivide");
+buttonDivide.addEventListener("click", divideFunction)
+
 const buttonMultiply = document.querySelector(".buttonMultiply");
+buttonMultiply.addEventListener("click", multiplyFunction)
+
 const buttonSubtract = document.querySelector(".buttonSubtract");
+buttonSubtract.addEventListener("click", subtractFunction);
+
 const buttonClear = document.querySelector(".buttonClear");
+buttonClear.addEventListener("click", clearFunction);
+
 const buttonDot = document.querySelector(".buttonDot");
+buttonDot.addEventListener("click", dotFunction);
+
 const buttonAdd = document.querySelector(".buttonAdd");
+buttonAdd.addEventListener("click", addFunction);
 
-buttonNumber.addEventListener("click", inputNumber);
 
-function inputNumber(){
-    console.log(buttonNumber.textContent);
+function enterFunction(){
+    secondNumber = display.textContent;
+    clearFunction();
+
+    if(mathFunction == 1)
+    {
+        answer = firstNumber / secondNumber;
+        display.append(answer);
+    }
+
+    mathFunction = 0;
+}
+
+function inputNumber(event){
+    display.append(event.target.innerText)
+}
+
+function divideFunction(){
+    firstNumber = display.textContent;
+    clearFunction();
+    mathFunction = 1;
+}
+
+function multiplyFunction(){
+
+    mathFunction
+}
+
+function subtractFunction(){
+
+}
+
+function clearFunction(){
+    while(display.firstChild)
+    {
+        display.removeChild(display.firstChild);
+    }
+}
+
+function dotFunction(){
+    
+}
+
+function addFunction(){
+
 }
