1f46d98715e602f2050a15be50b531dace6ae7ce
Kenny Sorensen <kenny.d.sorensen@gmail.com>
2024-06-25 15:08:08 -0400

    created a working decimal point function

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ let firstNumber = 0;
 let secondNumber = 0;
 let mathFunction = 0;
 let answer;
+let dotUsed;
 
 const container = document.querySelector(".container");
 const display = document.querySelector(".display");
@@ -40,9 +41,55 @@ function enterFunction(){
 
     if(mathFunction == 1)
     {
-        answer = firstNumber / secondNumber;
+        if(secondNumber == 0)
+        {
+            display.append("ERROR. Cannot divide by 0!");
+        }
+        else
+        {
+            answer = firstNumber / secondNumber;
+            answer = +answer.toFixed(2);
+            display.append(answer);
+        }
+    }
+
+    if(mathFunction == 2)
+    {
+        answer = firstNumber * secondNumber;
+        answer = +answer.toFixed(2);
         display.append(answer);
     }
+
+    if(mathFunction == 3)
+    {
+        answer = firstNumber - secondNumber;
+        answer = +answer.toFixed(2);
+        display.append(answer);
+    }
+
+    if(mathFunction == 4)
+    {
+        answer = +firstNumber + +secondNumber;
+        answer = +answer.toFixed(2);
+        display.append(answer);
+    }
+
+    for(i = 0; i < display.textContent.length; i++)
+        {
+            if(display.textContent.charAt(i) == '.')
+                {
+                    dotUsed = true;
+                }
+        }
+    
+    if(dotUsed)
+        {
+            buttonDot.setAttribute('disabled', 'disabled');
+        }
+    else
+        buttonDot.removeAttribute("disabled");
+
+    dotUsed = false;
 
     mathFunction = 0;
 }
@@ -58,12 +105,15 @@ function divideFunction(){
 }
 
 function multiplyFunction(){
-
-    mathFunction
+    firstNumber = display.textContent;
+    clearFunction();
+    mathFunction = 2;
 }
 
 function subtractFunction(){
-
+    firstNumber = display.textContent;
+    clearFunction();
+    mathFunction = 3;
 }
 
 function clearFunction(){
@@ -71,12 +121,31 @@ function clearFunction(){
     {
         display.removeChild(display.firstChild);
     }
+    buttonDot.removeAttribute('disabled');
 }
 
 function dotFunction(){
+    display.append(".");
+    for(i = 0; i < display.textContent.length; i++)
+        {
+            if(display.textContent.charAt(i) == '.')
+                {
+                    dotUsed = true;
+                }
+        }
     
+    if(dotUsed)
+        {
+            buttonDot.setAttribute('disabled', 'disabled');
+        }
+    else
+        buttonDot.removeAttribute("disabled");
+
+    dotUsed = false;
 }
 
 function addFunction(){
-
+    firstNumber = display.textContent;
+    clearFunction();
+    mathFunction = 4;
 }
